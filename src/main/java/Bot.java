117e302ea4d4872498c5c5f09a141fbae49190aa
Alexandr <razchurik@gmail.com>
2023-03-21 15:32:12 +0300

    token removed from code for now

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -5,7 +5,7 @@ import net.dv8tion.jda.api.entities.Activity;
 
 public class Bot {
     public static void main(String[] args) {
-        JDABuilder builder = JDABuilder.createDefault("MTA4NTE2NTUwMzIyOTc0MzExNA.GMeR-2.Btd_NG41KefaplOQSvQbYxYnDEqbsYUSqcQOxo");
+        JDABuilder builder = JDABuilder.createDefault("");
 
         // Set activity (like "playing Something")
         builder.setActivity(Activity.watching("Mr. Dyaika's youtube channel"));
