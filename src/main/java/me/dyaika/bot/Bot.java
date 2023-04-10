018c466e7a4490ae8b5bca79bd21524da56dec05
Alexandr <razchurik@gmail.com>
2023-04-10 23:20:37 +0300

    обавил комментарий

--- src/main/java/me/dyaika/bot/Bot.java
+++ src/main/java/me/dyaika/bot/Bot.java
@@ -34,13 +34,16 @@ public class Bot {
      */
     private static JsonObject guilds_json;
 
+    /**
+     * Основные команды бота
+     */
     private static List<SlashCommandData> slashCommands = new ArrayList<>();
 
     /**
      * Главная функция, запускает бота.
      * Здесь приложение получает токены доступа и выбирает, какие команды будут доступны
-     * @param args
-     * @throws IOException
+     * @param args Не используются
+     * @throws IOException Ошибки при чтении файлов и тд
      */
     public static void main(String[] args) throws IOException {
         // Подключение Discord
